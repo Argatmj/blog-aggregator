1c191a1bc4275292c349eed9a0c3bae6aa9f1000
Argatmj <ikomangarga0612@gmail.com>
2026-06-18 14:29:49 +0200

    Added config

---
+++ src/config.ts
@@ -0,0 +1,52 @@
+import fs, { read } from "fs";
+import os from "os";
+import path from "path";
+
+export type Config = {
+    dbUrl: string;
+    currentUserName: string;
+}
+
+function getConfigFilePath(): string {
+    const filePath = path.join(os.homedir(),".gatorconfig.json");
+    return filePath;
+}
+
+function validateConfig(rawConfig: any): Config {
+    if (
+        typeof rawConfig === "object" &&
+        rawConfig !== null &&
+        typeof rawConfig.db_url === "string" &&
+        typeof rawConfig.currentUserName === "string"
+    ) {
+    return rawConfig;
+  }
+  throw new Error("Invalid config file");
+}
+
+function writeConfig(cfg: Config): void {
+    const filePath = getConfigFilePath();
+    const cfgJson = JSON.stringify(cfg);
+    fs.writeFileSync(filePath, cfgJson);
+}
+
+export function setUser(name: string){
+    const currConfig = readConfig();
+    const config: Config = {
+        ...currConfig,
+        currentUserName: name
+    };
+    writeConfig(config);
+    console.log(config)
+
+}
+
+export function readConfig(): Config{
+    const filePath = getConfigFilePath();
+    const rawConfigJson = fs.readFileSync(filePath, {
+        encoding: "utf-8"
+    });
+    const rawConfig = JSON.parse(rawConfigJson);
+    return validateConfig(rawConfig);
+
+}
